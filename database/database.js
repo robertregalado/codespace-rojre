cb093f4b533b081079e9347834ff827989c24e1d
Robert J. Regalado <rjregalado1979@gmail.com>
2023-02-05 15:40:44 +0000

    added mongoose database

---
+++ database/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const MONGOBD_URL = process.env
+
+exports.connect = () => {
+    mongoose.connect(MONGOBD_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then()
+    .catch((error) => {
+        console.log(`DB connection FAILED`);
+        console.log(error);
+        process.env(1);
+    });
+}
